f4e2c13fa078d1005711f659f372a533869bfa2a
Henry <henry.rabina@gmail.com>
2018-07-08 23:08:15 +0300

    Added first buttons to the menu-screen.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,45 @@
+#include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <Main.hpp>
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(1800, 1000), "Pac-Man", sf::Style::Default);
+  sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pac-Man", sf::Style::Fullscreen);
   sf::Event event;
+  sf::Font font;
+  font.loadFromFile("Ubuntu-B.ttf");
+
+  sf::Text StartButton("Start Game", font, 100);
+  sf::Text ExitButton("Exit", font, 100);
+  sf::Text TitleText("Pac-Man", font, 200);
+  StartButton.setFillColor(sf::Color::Black);
+  ExitButton.setFillColor(sf::Color::Black);
+  TitleText.setFillColor(sf::Color::Red);
+  StartButton.setPosition(window.getSize().x/2-StartButton.getLocalBounds().width/2, window.getSize().y-StartButton.getLocalBounds().height*5);
+  ExitButton.setPosition(window.getSize().x/2-ExitButton.getLocalBounds().width/2, window.getSize().y-ExitButton.getLocalBounds().height*2);
+  TitleText.setPosition(window.getSize().x/2-TitleText.getLocalBounds().width/2, window.getSize().y-TitleText.getLocalBounds().height*7);
+  
   window.setFramerateLimit(60);
 
   while(window.isOpen())
     {
+      sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+      sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+      if (StartButton.getGlobalBounds().contains(mousePosF))
+	{
+	  StartButton.setFillColor(sf::Color::Blue);
+	}
+      else if (ExitButton.getGlobalBounds().contains(mousePosF))
+	{
+	  ExitButton.setFillColor(sf::Color::Blue);
+	}
+      else
+	{
+	  ExitButton.setFillColor(sf::Color::Black);
+	  StartButton.setFillColor(sf::Color::Black);
+	}
+ 
       while(window.pollEvent(event))
 	{
 	  switch(event.type)
@@ -16,10 +47,31 @@ int main()
 	    case sf::Event::Closed:
 	      window.close();
 	      break;
+
+	    case sf::Event::MouseButtonPressed:
+	      if (StartButton.getGlobalBounds().contains(mousePosF))
+		{
+		  std::cout << "clicked" << std::endl;
+		  Game(window, font);
+		}
+	      else if (ExitButton.getGlobalBounds().contains(mousePosF))
+		{
+		  window.close();
+		  break;
+		}
 	    }
 	}
       window.clear(sf::Color::White);
+      window.draw(StartButton);
+      window.draw(ExitButton);
+      window.draw(TitleText);
       window.display();
     }
+  return 0;
+}
+
+
+int Game(sf::RenderWindow &window, sf::Font font)
+{
   return 0;
 }
