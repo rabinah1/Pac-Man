3fef6a8fc7da97f83fda50bb4a412bba9bb8a03c
Henry <henry.rabina@gmail.com>
2018-07-10 11:39:07 +0300

    Added a game-screen that starts when start game-button is pressed in the menu. The game screen contains a pac-man character moving around.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
-#include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 #include <Main.hpp>
 
 int main()
@@ -44,14 +44,9 @@ int main()
 	{
 	  switch(event.type)
 	    {
-	    case sf::Event::Closed:
-	      window.close();
-	      break;
-
 	    case sf::Event::MouseButtonPressed:
 	      if (StartButton.getGlobalBounds().contains(mousePosF))
 		{
-		  std::cout << "clicked" << std::endl;
 		  Game(window, font);
 		}
 	      else if (ExitButton.getGlobalBounds().contains(mousePosF))
@@ -73,5 +68,108 @@ int main()
 
 int Game(sf::RenderWindow &window, sf::Font font)
 {
+  int control = 1;
+  int speed = 4;
+  int counter = 0;
+  int index = 0;
+  sf::Event event;
+  sf::Texture pac1;
+  sf::Texture pac2;
+  sf::Texture pac3;
+  sf::Texture pac4;
+  sf::Texture pac5;
+  pac1.loadFromFile("Pac1.png");
+  pac2.loadFromFile("Pac2.png");
+  pac3.loadFromFile("Pac3.png");
+  pac4.loadFromFile("Pac4.png");
+  pac5.loadFromFile("Pac5.png");
+  pac1.setSmooth(true);
+  pac2.setSmooth(true);
+  pac3.setSmooth(true);
+  pac4.setSmooth(true);
+  pac5.setSmooth(true);
+  std::vector<sf::Texture> textureVector;
+  textureVector.push_back(pac1);
+  textureVector.push_back(pac2);
+  textureVector.push_back(pac3);
+  textureVector.push_back(pac4);
+  textureVector.push_back(pac5);
+  textureVector.push_back(pac4);
+  textureVector.push_back(pac3);
+  textureVector.push_back(pac2);
+  textureVector.push_back(pac1);
+  int len = textureVector.size();
+
+  sf::Sprite mySprite;
+  mySprite.setTexture(pac5);
+  mySprite.setOrigin(mySprite.getOrigin().x+mySprite.getLocalBounds().width/2, mySprite.getOrigin().y+mySprite.getLocalBounds().height/2);
+  mySprite.setRotation(180);
+  window.setFramerateLimit(60);
+
+  while(window.isOpen())
+    {
+      if (counter == 2)
+	{
+	  counter = 0;
+	  if (index == len)
+	    {
+	      index = 0;
+	    }
+	  mySprite.setTexture(textureVector[index]);
+	  index += 1;
+	}
+      counter = counter + 1;
+      if(control == 1)
+	{
+	  mySprite.setRotation(0);
+	  mySprite.setPosition(sf::Vector2f(mySprite.getPosition().x+speed, mySprite.getPosition().y));
+	}
+      else if(control == 2)
+	{
+	  mySprite.setRotation(180);
+	  mySprite.setPosition(sf::Vector2f(mySprite.getPosition().x-speed, mySprite.getPosition().y));
+	}
+      else if(control == 3)
+	{
+	  mySprite.setRotation(270);
+	  mySprite.setPosition(sf::Vector2f(mySprite.getPosition().x, mySprite.getPosition().y-speed));
+	}
+      else if(control == 4)
+	{
+	  mySprite.setRotation(90);
+	  mySprite.setPosition(sf::Vector2f(mySprite.getPosition().x, mySprite.getPosition().y+speed));
+	}
+      while(window.pollEvent(event))
+	{
+	  switch(event.type)
+	    {
+	    case sf::Event::KeyPressed:
+	      if(event.key.code == sf::Keyboard::Escape)
+		{
+		  window.close();
+		  break;
+		}
+	      else if(event.key.code == sf::Keyboard::Up)
+		{
+		  control = 3;
+		}
+	      else if(event.key.code == sf::Keyboard::Down)
+		{
+		  control = 4;
+		}
+	      else if(event.key.code == sf::Keyboard::Left)
+		{
+		  control = 2;
+		}
+	      else if(event.key.code == sf::Keyboard::Right)
+		{
+		  control = 1;
+		}
+	    }
+	}
+      window.clear(sf::Color::Black);
+      window.draw(mySprite);
+      window.display();
+    }
   return 0;
 }
